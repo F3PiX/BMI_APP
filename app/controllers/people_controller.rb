a3179beaa8c7a7bdbcfd708f296457a7472c3d6f
F3PiX <F3PiXnl@gmail.com>
2015-07-29 16:16:43 +0200

    Destroyed and generated new PeopleController with correct sing and plural filenames

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,16 +1,21 @@
 class PeopleController < ApplicationController
+
+  # GET /people
   def index
     @people = Person.all
   end
 
+  # GET /people/1
   def show
     @person = Person.find(params[:id])
   end
 
+  # GET /people/new
   def new
     @person = Person.new
   end
 
+  #POST /people
   def create
     @person = Person.create(person_params)
 
@@ -18,7 +23,7 @@ class PeopleController < ApplicationController
   end
 
   private
-
+  # Use callbacks like : before_action to share common setup or constraints between actions
   def person_params
     params.require(:person).permit(:name, :length)
   end
