f668ed12fa8c3bfec5238b641093c544462ab6bb
F3PiX <F3PiXnl@gmail.com>
2015-07-29 16:13:29 +0200

    Adding ruby calculation BMI

--- calcfile.rb
+++ calcfile.rb
@@ -1,12 +1,9 @@
-#require 'bigdecimal'
-
-#height = (1.90).abs
-#height = height.to_i
-#sq_height = height * height 
-#puts sq_height
+# bmi = weight (k) / length^2 (m) #@person.measurement_today.weight
 
 weight = 80
 bmi_calc = weight/(1.90)**2
-puts bmi_calc
+puts bmi_calc.round(0)
+
+
 
 
