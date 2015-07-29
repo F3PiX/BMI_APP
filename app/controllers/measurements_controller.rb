adb8f4f7e20d7ba3e23bebf12b6a85c6ba4d1bc5
F3PiX <f3pixnl@gmail.com>
2015-07-29 23:31:16 +0200

    Completing measurements and decluttering

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,10 +1,9 @@
 class MeasurementsController < ApplicationController
-  #before_action :bmi_calc, only: :show
 
   # GET /measurements/1
   def show
-    @your_weight = Measurement.find(params[:id]).weight
-    bmi_calc(@your_weight)
+    your_weight = Measurement.find(params[:id]).weight
+    @calculated_bmi = bmi_calc(your_weight)
   end
 
   # GET /measurements/new
@@ -15,17 +14,18 @@ class MeasurementsController < ApplicationController
   # POST /measurements
   def create
   @measurement = Measurement.create(measurement_params)
-    redirect_to @measurement, notice: "Alweer een stap in de goede richting"
+  #notice laten zien
+  redirect_to @measurement, notice: "Alweer een stap in de goede richting"
   end
 
   def bmi_calc(weging)
-    weight = weging
     # input: weight from @measurement , height from @person
-    # returns bmi-number rounded to 0 dec
-    #@person.measurement_today.weight#TODO get Person.length for current user
-    #@bmi_calc = 25 # bmi = weight (k) / length^2 (m)
-    @bmi_calc = @your_weight / (1.96)**2
-    @bmi_calc.round(1)
+    # returns bmi-number
+    #@person.measurement_today.weight
+    #TODO get Person.length for current user
+    # bmi = weight (k) / length^2 (m)
+    bmi_calc = weging / (1.96)**2
+    bmi_calc.round(1)
   end
 
 
