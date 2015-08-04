ba45b752f06b36db6efd27d547b2e713bde6781b
F3PiX <F3PiXnl@gmail.com>
2015-08-04 23:05:42 +0200

    Lesson André tuesday 4/8

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,44 +1,26 @@
 class MeasurementsController < ApplicationController
+  before_action :current_user , only: [:new, :create]
 
-  # GET /measurements/1
-  def show
-    # #your_weight = Measurement.find(params[:id]).weight
-    # @current_user = Person.find(params[:person_id])
-    # your_weight = @current_user.measurements.last.weight
-    # @calculated_bmi = bmi_calc(your_weight)
-  end
+  # GET people/1/measurements/1
+  #def show
+  #end
 
-  # GET /measurements/new
+  # GET people/1/measurements/new
   def new
-    @current_user = Person.find(params[:person_id])
+    #@current_user = Person.find(params[:person_id]) #now in callback
     @measurement = @current_user.measurements.new
   end
 
-  # POST /measurements
+  # POST people/1/measurements
   def create
-    #create pattern w feedback on failing save (eg by validation)
-    #@current_user = Person.find(params[:person_id])
-    # if @measurement.save
-    #     #TODO notice laten zien
-    #     #redirect_to people_path(@current_user), notice: "Alweer een stap in de goede richting"
-    #   redirect_to '/'
-    #   else
-    #     render :new
-    # end
-    @current_user = Person.find(params[:person_id])
+    #@current_user = Person.find(params[:person_id]) #now in callback
     @measurement = @current_user.measurements.create!(measurement_params)
     redirect_to person_path(@current_user)
     #redirect_to @current_user
   end
 
-  def bmi_calc(weging)
-    # input: weight from @measurement , height from @person
-    # returns bmi-number with 1 dec
-    #@person.measurement_today.weight
-    #TODO get Person.length for current user
-    # bmi = weight (k) / length^2 (m)
-    bmi_calc = weging / (1.96)**2
-    bmi_calc.round(1)
+  def current_user
+    @current_user = Person.find(params[:person_id])
   end
 
 
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,7 +2,7 @@
 
 class PeopleController < ApplicationController
   include BmiCalculationsHelper
-  before_action :current_user , only: [:show, :new]
+  # before_action :current_user , only: [:show, :new]
 
   # GET /people
   def index
@@ -11,19 +11,21 @@ class PeopleController < ApplicationController
 
   # GET /people/1
   def show
+    @current_measurement = @current_user.current_measurement
+    @latest_bmi = @current_measurement.bmi
     #@current_user = Person.find(params[:id])
     #@current_user = Person.find_by_name("Mozes Kriebel")
-
-    @latest_bmi = bmi_calc(@current_user) #if @current_user.measurements.last
+    @current_measurement = @current_user.measurements.first
+    @latest_bmi = bmi_calc(@current_measurement) #if @current_user.measurements.last
     #history should it come from elsewhere (helper?) now: in view.
-    #todo: iterate over bmi_history to calc bmi's per measurement
+    #todo: iterate over bmi_history to calc bmi's per measurement #Q : or in view's table?
     @bmi_history = @current_user.measurements
   end
 
   # GET /people/new
   def new
-    #@person = Person.new
-    @person = Person.find_by(params[:name]) || Person.new
+    @person = Person.new
+    #@person = Person.find_by(params[:name]) || Person.new
   end
 
   #POST /people
@@ -33,15 +35,17 @@ class PeopleController < ApplicationController
     redirect_to people_path
   end
 
-  def current_user
-    @current_user = Person.find(params[:id])
-  end
 
   private
   # Use callbacks like : before_action to share common setup or constraints between actions
   def person_params
     params.require(:person).permit(:name, :length)
   end
+
+  def current_user
+    @current_user = Person.find(params[:id])
+  end
+
 end
 
 
