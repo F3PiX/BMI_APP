6e6d18c825931eaa1662fb047469b292c45271b5
F3PiX <F3PiXnl@gmail.com>
2015-08-08 11:21:17 +0200

    Adding name search form and query

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,10 @@
 class Person < ActiveRecord::Base
   validates :name, presence: true
 
-
   has_many :measurements, dependent: :destroy
+
+  def self.search(search)
+    #LIKE works only For SQLIte, for PostgreSql use ILIKE
+    where("name LIKE ?", "%#{search}%")
+  end
 end
