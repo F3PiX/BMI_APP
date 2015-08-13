097f276e7b400448b88fe8262080566705ad63f9
F3PiX <F3PiXnl@gmail.com>
2015-08-13 18:39:28 +0200

    Adding before-block and context to Measurement model tests

--- spec/models/measurement_spec.rb
+++ spec/models/measurement_spec.rb
@@ -1,60 +1,51 @@
 require 'rails_helper'
 
 describe Measurement do
+  before :each do
+    @person = Person.new(name: 'Joop ter Heul', height: 1.80)
+  end
+
   it 'is valid with a data and a weight' do
-    person = Person.new(name: 'Joop ter Heul', height: 1.80)
-    measurement = person.measurements.new(weight: 73, date: Date.today )
+    measurement = @person.measurements.new(weight: 73, date: Date.today )
     expect(measurement).to be_valid
   end
 
   it 'is invalid without a date' do
-    person = Person.new(name: 'Joop ter Heul', height: 1.80)
-    measurement = person.measurements.new(weight: 73, date: nil )
+    measurement = @person.measurements.new(weight: 73, date: nil )
     measurement.valid?
     expect(measurement.errors[:date]).to include("can't be blank")
   end
 
   it 'is invalid without a weight' do
-    person = Person.new(
-      name: 'Joop ter Heul',
-      height: 1.80 )
-    measurement = person.measurements.new(
-      weight: nil,
-      date: Date.today )
+    measurement = @person.measurements.new(weight: nil, date: Date.today )
     measurement.valid?
     expect(measurement.errors[:weight]).to include("can't be blank")
   end
+
   it 'is invalid with a weight < 40' do
-    person = Person.new(
-      name: 'Joop ter Heul',
-      height: 1.80 )
-    measurement = person.measurements.new(
-      weight: 30,
-      date: Date.today )
+    measurement = @person.measurements.new(weight: 30, date: Date.today )
     measurement.valid?
     expect(measurement.errors[:weight]).to include("A BMI based on this weight is not a reliable measure")
   end
+
   it 'is invalid with a weight > 150' do
-    person = Person.new(
-      name: 'Joop ter Heul',
-      height: 1.80 )
-    measurement = person.measurements.new(
-      weight: 200,
-      date: Date.today )
+    measurement = @person.measurements.new(weight: 200, date: Date.today )
     measurement.valid?
     expect(measurement.errors[:weight]).to include("A BMI based on this weight is not a reliable measure")
   end
+
   it 'belongs to a person'
-  it 'returns bmi based on weight and height' do
-    person = Person.new(
-        name: 'Joop ter Heul',
-        height: 1.80 )
-    measurement = person.measurements.new(
-        weight: 75,
-        date: Date.today )
-    expect(measurement.bmi_calc).not_to eq 23.5
-    expect(measurement.bmi_calc).to eq 23.1
-  end
+
+
+    context "with calculated bmi" do
+      before :each do
+      @measurement =@person.measurements.new(weight: 75, date: Date.today)
+    end
+      it 'returns correct bmi based on weight and height' do
+        expect(@measurement.bmi_calc).not_to eq 23
+        expect(@measurement.bmi_calc).to eq 23.1
+      end
+    end
 end
 
 
