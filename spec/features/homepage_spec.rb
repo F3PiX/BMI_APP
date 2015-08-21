745889c989f0e26a39565d554bfaf7fbaa61e85c
F3PiX <F3PiXnl@gmail.com>
2015-08-21 17:24:58 +0200

    Adding homepage visit

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,22 +1,8 @@
 require 'rails_helper'
 
-feature 'my feature' do
-  background do
-
-  end
-
-  scenario 'my first test' do
-
-  end
-end
-
-#MEMO: temp out ; firefox not installed
-
-#Thijs: "check homepage only to see if there is SOMETHING showing up. With non changing text"
 feature 'Homepage' do
   scenario 'visit the homepage' do
     visit '/'
-    save_and_open_page
     expect(page).to have_content 'Who are you'
   end
 end
