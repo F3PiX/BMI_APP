54757ddce7f7aa78fd498b2b057bceee732a7990
F3PiX <F3PiXnl@gmail.com>
2015-08-21 17:23:53 +0200

    Adding new user specs

--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -1,20 +1,17 @@
 require 'rails_helper'
 
-#MEMO temp out while learning basic testing
-#MEMO Selenium not running in Chrome
+feature Person do
+  scenario 'is a new user' do
+    visit '/'
+    click_on('I am new here')
+    expect(page).to have_content('your name')
+    # expect{
+       fill_in "name", with: 'pietje precies'
+       fill_in 'height', with: 1.75
+       click_on "That's all"
+    # }.to change(Person, :count).by(1) #TODO check DBCleaner settings
+    expect(page).to have_content "Add your weight"
 
-# feature 'People' do
-#   scenario 'create a person' do
-#     visit '/people'
-#     expect(page).to have_content 'My people'
-#     click_link 'New person'
-#
-#     expect(page).to have_content 'Person#new'
-#     fill_in 'Name', :with => 'Maud'
-#     fill_in 'Length', :with => '1.75'
-#     click_button 'Create Person'
-#
-#     expect(page).to have_content 'My people'
-#     expect(page).to have_content 'Maud'
-#   end
-# end
+    save_and_open_screenshot
+ end
+end
