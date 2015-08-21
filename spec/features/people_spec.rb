10ec75e1309b56f8e1552283a9fcd8f97e1fa029
F3PiX <F3PiXnl@gmail.com>
2015-08-21 21:02:46 +0200

    Adding person features, stubbed DBCleaner config

--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -1,27 +1,51 @@
 require 'rails_helper'
 
 feature Person do
-  scenario 'is a new user' do
-    visit '/'
-    click_on('I am new here')
-    expect(page).to have_content('your name')
-    # expect{
-       fill_in "name", with: 'pietje precies'
-       fill_in 'height', with: 1.75
-       click_on "That's all"
-    # }.to change(Person, :count).by(1) #TODO check DBCleaner settings
-    expect(page).to have_content "Add your weight"
-
-    #save_and_open_screenshot
+  background do
+    person = Person.create!(name: "Miep", height: 1.65)
   end
-  scenario 'is a new user with cold feet' do
-    visit '/'
-    click_on('I am new here')
-    expect(page).to have_content('your name')
-    click_on "I don't want this"
-    expect(page).to have_content 'Who are you?'
+
+  context 'is an existing user' do
+    scenario 'who wants to access his data' do
+      person = FactoryGirl.create(:person)
+      visit '/people'
+      expect(page).to have_content "Who are you"
+      expect(page).to have_content person.name
     end
+  end
+
+  context 'is a new user' do
+    scenario 'who wants to join' do
+      visit '/'
+      click_on('I am new here')
+      expect(page).to have_content('your name')
+      #expect{
+         fill_in "name", with: 'pietje precies'
+         fill_in 'height', with: 1.75
+         click_on "That's all"
+      #}.to change(Person, :count).by(1) #TODO check DBCleaner settings
+      expect(page).to have_content "Add your weight"
+
+      #save_and_open_screenshot
+    end
+    scenario 'with cold feet' do
+      visit '/'
+      click_on('I am new here')
+      expect(page).to have_content('your name')
+      click_on "I don't want this"
+      expect(page).to have_content 'Who are you?' #people/index
+    end
+
+    scenario 'who pushes all the wrong buttons' do
+     visit '/'
+     click_on('I am new here')
+     expect(page).to have_content('your name')
+     click_on "That's all"
+     expect(page).to have_content "can't be blank"
+    end
+  end
 end
+
 
 
 #MEMO temp out while learning basic testing
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,11 +43,12 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with :truncation
   end
 
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-      end
-    end
+  #I stubbed this, because my testperson didn't live through the tests
+  # config.around(:each) do |example|
+  #   DatabaseCleaner.cleaning do
+  #     example.run
+  #     end
+  #   end
 
   # config.before(:each) do |example|
   #   DatabaseCleaner.strategy= example.metadata[:js] ? :truncation : :transaction
