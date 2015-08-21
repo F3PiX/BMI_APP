55d3a5eb9f6f318ddb82f15022027ed3ac4a6adb
F3PiX <F3PiXnl@gmail.com>
2015-08-21 18:02:58 +0200

    Test user w cold feet

--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -12,6 +12,33 @@ feature Person do
     # }.to change(Person, :count).by(1) #TODO check DBCleaner settings
     expect(page).to have_content "Add your weight"
 
-    save_and_open_screenshot
- end
+    #save_and_open_screenshot
+  end
+  scenario 'is a new user with cold feet' do
+    visit '/'
+    click_on('I am new here')
+    expect(page).to have_content('your name')
+    click_on "I don't want this"
+    expect(page).to have_content 'Who are you?'
+    end
 end
+
+
+#MEMO temp out while learning basic testing
+#MEMO Selenium not running in Chrome
+
+# feature 'People' do
+#   scenario 'create a person' do
+#     visit '/people'
+#     expect(page).to have_content 'My people'
+#     click_link 'New person'
+#
+#     expect(page).to have_content 'Person#new'
+#     fill_in 'Name', :with => 'Maud'
+#     fill_in 'Length', :with => '1.75'
+#     click_button 'Create Person'
+#
+#     expect(page).to have_content 'My people'
+#     expect(page).to have_content 'Maud'
+#   end
+# end
